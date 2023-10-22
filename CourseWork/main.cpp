141e765a227f012bdbb5efe384994f8657746e8d
Artem <ediskrad11111@gmail.com>
2023-10-22 19:21:19 +0300

    second commit

--- CourseWork/main.cpp
+++ CourseWork/main.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <iostream>
+#include "pqxx/pqxx"
 
 enum EngineType
 {
@@ -138,3 +140,100 @@ public:
     bool getHasADS() { return hasADS; }
     void setHasADS(bool hasADS) { this->hasADS = hasADS; }
 };
+
+
+int main() {
+    try {
+        pqxx::connection conn("your_connection_string_here");
+
+        int choice;
+        while (true) {
+            std::cout << "Выберите тип транспорта:" << std::endl;
+            std::cout << "1 - Bus" << std::endl;
+            std::cout << "2 - Trolleybus" << std::endl;
+            std::cout << "3 - Taxi" << std::endl;
+            std::cout << "4 - Car Sharing" << std::endl;
+            std::cout << "0 - Выход" << std::endl;
+            std::cout << "Ваш выбор: ";
+            std::cin >> choice;
+
+            if (choice == 0) {
+                break;
+            }
+
+            pqxx::work txn(conn);
+            pqxx::result result;
+
+            switch (choice) {
+                case 1:
+                    result = txn.exec("SELECT * FROM bus");
+                    break;
+                case 2:
+                    result = txn.exec("SELECT * FROM trolleybus");
+                    break;
+                case 3:
+                    result = txn.exec("SELECT * FROM taxi");
+                    break;
+                case 4:
+                    result = txn.exec("SELECT * FROM car_sharing");
+                    break;
+                default:
+                    std::cout << "Incorrect input." << std::endl;
+                    continue;
+            }
+
+            txn.commit();
+
+            for (pqxx::result::size_type i = 0; i < result.size(); ++i) {
+                std::string brand = result[i]["brand"].as<std::string>();
+                std::string model = result[i]["model"].as<std::string>();
+                std::string color = result[i]["color"].as<std::string>();
+                EngineType engineType = static_cast<EngineType>(result[i]["engineType"].as<int>());
+
+                if (choice == 1) {
+                    int capacity = result[i]["capacity"].as<int>();
+                    std::string from = result[i]["from_location"].as<std::string>();
+                    std::string to = result[i]["to_location"].as<std::string>();
+                    std::string frequency = result[i]["frequency"].as<std::string>();
+                    bool hasContactlessPayment = result[i]["has_contactless_payment"].as<bool>();
+
+                    Bus bus(brand, model, color, engineType, capacity, from, to, frequency, hasContactlessPayment);
+
+                }
+                else if (choice == 2) {
+                    int capacity = result[i]["capacity"].as<int>();
+                    std::string from = result[i]["from_location"].as<std::string>();
+                    std::string to = result[i]["to_location"].as<std::string>();
+                    std::string frequency = result[i]["frequency"].as<std::string>();
+                    bool hasSockets = result[i]["has_sockets"].as<bool>();
+
+                    TrolleyBus trolleybus(brand, model, color, engineType, capacity, from, to, frequency, hasSockets);
+                }
+                else if (choice == 3) {
+                    double pricePerKil = result[i]["price_per_kil"].as<double>();
+                    double rating = result[i]["rating"].as<double>();
+                    bool hasDriver = result[i]["has_driver"].as<bool>();
+                    bool hasWiFi = result[i]["has_wifi"].as<bool>();
+                    bool hasChildSeat = result[i]["has_child_seat"].as<bool>();
+                    RentCarTypes rentCarType = static_cast<RentCarTypes>(result[i]["rent_car_type"].as<int>());
+
+                    Taxi taxi(brand, model, color, engineType, pricePerKil, rating, hasDriver, hasWiFi, hasChildSeat, rentCarType);
+
+                }
+                else if (choice == 4) {
+                    double pricePerMinute = result[i]["price_per_minute"].as<double>();
+                    bool hasInsurance = result[i]["has_insurance"].as<bool>();
+                    bool hasADS = result[i]["has_ads"].as<bool>();
+
+                    CarSharing carSharing(brand, model, color, engineType, pricePerMinute, hasInsurance, hasADS);
+
+                }
+            }
+        }
+    } catch (const std::exception &e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
